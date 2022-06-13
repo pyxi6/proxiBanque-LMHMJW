e21d78af568c926a471e39f9d5bb79478ed0b07e
willaume.jacques <willaume.jacques@gmail.com>
2022-06-13 15:06:08 +0200

    getter setter + constructeur

--- src/main/java/com/formation/proxibanque/lmhmjw/dto/VirementRequestDTO.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/dto/VirementRequestDTO.java
@@ -10,4 +10,25 @@ public class VirementRequestDTO {
     private Long codeSource;
     private Long codeDestination;
     private double montant;
+    
+	public Long getCodeSource() {
+		return codeSource;
+	}
+	public void setCodeSource(Long codeSource) {
+		this.codeSource = codeSource;
+	}
+	public Long getCodeDestination() {
+		return codeDestination;
+	}
+	public void setCodeDestination(Long codeDestination) {
+		this.codeDestination = codeDestination;
+	}
+	public double getMontant() {
+		return montant;
+	}
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+    
+    
 }
--- src/main/java/com/formation/proxibanque/lmhmjw/entity/Compte.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/entity/Compte.java
@@ -9,12 +9,14 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+//@Inheritance(strategy = InheritanceType.JOINED)
 @Data  // creation des gether setter via l'extention Lambok
 @NoArgsConstructor // constructeur sans params
 @AllArgsConstructor // Constructeur avec tous les args
-public class Compte {
+public abstract class Compte {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //incrim valeur +1
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //incrim valeur +1
     private Long code;
     private double solde;
     private Date dateCreation;
@@ -23,4 +25,53 @@ public class Compte {
                                 // Ordinaire c'est sous forme numeriques
     private TypeCompte type;
 
+	public Compte() {
+	}
+
+	public Compte(Long code, double solde, Date dateCreation, TypeCompte type) {
+
+		this.code = code;
+		this.solde = solde;
+		this.dateCreation = dateCreation;
+		this.type = type;
+	}
+
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public TypeCompte getType() {
+		return type;
+	}
+
+	public void setType(TypeCompte type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Compte [code=" + code + ", solde=" + solde + ", dateCreation=" + dateCreation + ", type=" + type + "]";
+	}
+    
+    
 }
