30d323e2169731ce71c8f80d457a7502dfbdc2b7
pixi6 <hocine.menhouk@gmail.com>
2022-06-14 15:27:49 +0200

    commit sur la branche devHocine Par securite, pour etre sur d'etre a jour

--- src/main/java/com/formation/proxibanque/lmhmjw/CompteServiceLmhmjwApplication.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/CompteServiceLmhmjwApplication.java
@@ -2,8 +2,11 @@ package com.formation.proxibanque.lmhmjw;
 
 import com.formation.proxibanque.lmhmjw.entity.Adresse;
 import com.formation.proxibanque.lmhmjw.entity.Customer;
+import com.formation.proxibanque.lmhmjw.repository.CompteRepository;
 import com.formation.proxibanque.lmhmjw.repository.CustomerRepository;
 
+import com.formation.proxibanque.lmhmjw.repository.OpperationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,32 +15,19 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CompteServiceLmhmjwApplication {
 
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private CompteRepository compteRepository;
+	@Autowired
+	private OpperationRepository opperationRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CompteServiceLmhmjwApplication.class, args);
 	}
 
-	// creation d'un Bean pour tester si les info s'enregistre dans la base
-	// le bean se lance à la compilation
-//	@Bean
-//	CommandLineRunner Start(CompteRepository compteRepository) {
-//		return args -> {
-//			
-//			compteRepository.save(new Compte(null, 10000, new Date(), TypeCompte.COURANT));
-//			compteRepository.save(new Compte(null, 20500, new Date(), TypeCompte.EPARGNE));
-//			compteRepository.save(new Compte(null, 3070, new Date(), TypeCompte.COURANT));
-//			// finAll recupere tous les compte
-//			// foreach parcour la liste
-//			compteRepository.findAll().forEach(cp -> {
-//				System.out.println(cp.getType());
-//				System.out.println(cp.getSolde());
-//
-//			});
-//
-//		};		
-//
-//	}
-	
-	Adresse adresse1 = new Adresse("8 rue Perlimpimpin", 92800, "Puteaux");
+
+	/*Adresse adresse1 = new Adresse("8 rue Perlimpimpin", 92800, "Puteaux");
 	Adresse adresse2 = new Adresse("7 rue Delay", 92100, "Boulogne");
 	Adresse adresse3 = new Adresse("11 rue de la Liberté", 95620, "Parmain");
 
@@ -50,6 +40,8 @@ public class CompteServiceLmhmjwApplication {
 			customerRepository.save(new Customer(2L, "Karim", "BenHakhlef", "0123625648", adresse2));
 			customerRepository.save(new Customer(3L, "Etienne", "Dufour", "0178654778", adresse3));
 
+
+
 			// finAll recupere tous les compte
 			// foreach parcour la liste
 			customerRepository.findAll().forEach(customer -> {
@@ -59,12 +51,11 @@ public class CompteServiceLmhmjwApplication {
 				System.out.println(customer.getNumTel());
 				System.out.println(customer.getAdresse());
 
-
 			});
 
 		};		
 
-	}
+	}*/
 	
 	
 }
--- src/main/java/com/formation/proxibanque/lmhmjw/service/CompteService.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/service/CompteService.java
@@ -1,8 +1,18 @@
 package com.formation.proxibanque.lmhmjw.service;
 
+import com.formation.proxibanque.lmhmjw.entity.Compte;
+import com.formation.proxibanque.lmhmjw.entity.Opperation;
+import org.springframework.data.domain.Page;
+
 public interface CompteService {
+
+    public Compte consulterCompte(Long codeCpte);
+    public void versement (Long codeCpte, double montant);
+    public  void retrait(Long codeCpte, double montant);
 
     // methode pour effectuer un virement
     // en pararm l'Id du compte qui effect le vireù, celui qui le recoi, le montant du virement
-     void virement(Long codeSource, Long codeDestination, double montant);
+     public void virement(Long codeSource, Long codeDestination, double montant);
+
+     public Page<Opperation> listOpperation (Long CodeCpte, int page, int size);
 }
