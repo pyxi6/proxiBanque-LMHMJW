2456900c9a50fe3915122b1a98461039c2e727ab
Laurane_M <laurane.magne@gmail.com>
2022-06-15 14:27:18 +0100

    pour merge avant debogage comptes

--- src/main/java/com/formation/proxibanque/lmhmjw/CompteServiceLmhmjwApplication.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/CompteServiceLmhmjwApplication.java
@@ -20,37 +20,37 @@ public class CompteServiceLmhmjwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompteServiceLmhmjwApplication.class, args);
-	}
-
-	
-	Adresse adresse1 = new Adresse("8 rue Perlimpimpin", "92800", "Puteaux");
-	Adresse adresse2 = new Adresse("7 rue Delay", "92100", "Boulogne");
-	Adresse adresse3 = new Adresse("11 rue de la Liberté", "95620", "Parmain");
-
-
-	
-	@Bean
-	CommandLineRunner Start(CustomerRepository customerRepository) {
-		return args -> {
-			
-			customerRepository.save(new Customer(1L, "Jean", "DeLaFontaine", "0123654778", adresse1));
-			customerRepository.save(new Customer(2L, "Karim", "BenHakhlef", "0123625648", adresse2));
-			customerRepository.save(new Customer(3L, "Etienne", "Dufour", "0178654778", adresse3));
-
-
-			// finAll recupere tous les compte
-			// foreach parcour la liste
-			customerRepository.findAll().forEach(customer -> {
-				System.out.println(customer.getId());
-				System.out.println(customer.getNom());
-				System.out.println(customer.getPrenom());
-				System.out.println(customer.getNumTel());
-				System.out.println(customer.getAdresse());
-
-
-			});
-
-		};		
+//	}
+//
+//	
+//	Adresse adresse1 = new Adresse("8 rue Perlimpimpin", "92800", "Puteaux");
+//	Adresse adresse2 = new Adresse("7 rue Delay", "92100", "Boulogne");
+//	Adresse adresse3 = new Adresse("11 rue de la Liberté", "95620", "Parmain");
+//
+//
+//	
+//	@Bean
+//	CommandLineRunner Start(CustomerRepository customerRepository) {
+//		return args -> {
+//			
+//			customerRepository.save(new Customer(1L, "Jean", "DeLaFontaine", "0123654778", adresse1));
+//			customerRepository.save(new Customer(2L, "Karim", "BenHakhlef", "0123625648", adresse2));
+//			customerRepository.save(new Customer(3L, "Etienne", "Dufour", "0178654778", adresse3));
+//
+//
+//			// finAll recupere tous les compte
+//			// foreach parcour la liste
+//			customerRepository.findAll().forEach(customer -> {
+//				System.out.println(customer.getId());
+//				System.out.println(customer.getNom());
+//				System.out.println(customer.getPrenom());
+//				System.out.println(customer.getNumTel());
+//				System.out.println(customer.getAdresse());
+//
+//
+//			});
+//
+//		};		
 	}
 	
 	
