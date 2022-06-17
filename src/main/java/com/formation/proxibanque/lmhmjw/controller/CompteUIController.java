770ec14bb65334ac0b28669c04e6b5c013cab860
willaume.jacques <willaume.jacques@gmail.com>
2022-06-17 17:24:57 +0200

    front

--- src/main/java/com/formation/proxibanque/lmhmjw/controller/CompteUIController.java
+++ src/main/java/com/formation/proxibanque/lmhmjw/controller/CompteUIController.java
@@ -1,11 +1,16 @@
 package com.formation.proxibanque.lmhmjw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.formation.proxibanque.lmhmjw.repository.CompteCourantRepository;
 import com.formation.proxibanque.lmhmjw.repository.CompteEpargneRepository;
 import com.formation.proxibanque.lmhmjw.repository.CompteRepository;
 
+@Controller
 public class CompteUIController {
 	
 	  @Autowired
@@ -25,7 +30,19 @@ public class CompteUIController {
 			this.compteEpargneRepository = compteEpargneRepository;
 		}
 		
-		public 
+		@GetMapping("/virement/courant/{id}")
+		public String LoadVirementFromCourant(@PathVariable Long id, Model model) {
+			model.addAttribute("compte", compteCourantRepository.findById(id));
+			return "virement";
+		}
+		
+		@GetMapping("/virement/epargne/{id}")
+		public String LoadVirementFromEpargne(@PathVariable Long id, Model model) {
+			model.addAttribute("compte", compteEpargneRepository.findById(id));
+			return "virement";
+		}
+		
+		
 	    
 	    
 }
